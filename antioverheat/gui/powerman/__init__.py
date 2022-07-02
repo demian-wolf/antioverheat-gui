957dbde42ff77e4e7d5d6f5bed7371a14a7ccf12
Demian Volkov <demianwolfssd@gmail.com>
2022-07-03 00:43:53 +0300

    Fix setting `auto_adjust` from CLI

--- antioverheat/gui/powerman/__init__.py
+++ antioverheat/gui/powerman/__init__.py
@@ -8,7 +8,7 @@ from .scale import FrequencyScale
 class PowerManager(tk.Tk):
     """The main class of this part of this app."""
 
-    def __init__(self, auto_adjust=False):
+    def __init__(self, auto_adjust: bool=False):
         super(PowerManager, self).__init__()
 
         self.update_idletasks()
@@ -17,7 +17,10 @@ class PowerManager(tk.Tk):
         self.attributes("-topmost", True)
 
         FrequencyScale(self).grid(row=0, columnspan=2)
-        AutoAdjustControls(self).grid(row=1, columnspan=2)
+        AutoAdjustControls(
+            self,
+            auto_adjust=auto_adjust,
+        ).grid(row=1, columnspan=2)
 
         tk.Button(
             self,
